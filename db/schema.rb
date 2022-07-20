b3256f3e4a240ecfa6223f207d2112de066e3612
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-20 19:07:10 +0900

    Add: モデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_07_20_100320) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.datetime "sidekiq_executed_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
