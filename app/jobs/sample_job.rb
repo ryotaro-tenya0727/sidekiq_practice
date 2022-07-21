77bc5353b83dcb5160993527444411d7cf7279d1
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-22 07:27:35 +0900

    Change: sleep削除

--- app/jobs/sample_job.rb
+++ app/jobs/sample_job.rb
@@ -2,7 +2,6 @@ class SampleJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-      sleep(1)
       Message.find_each do |message|
       message.update(sidekiq_executed_time: DateTime.current)
     end
