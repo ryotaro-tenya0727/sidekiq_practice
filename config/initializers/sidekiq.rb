f89fe879974aaa8da931237cf3a684639a610b18
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-22 13:15:47 +0900

    Change: 修正

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", 'redis://localhost:6379')}
+  config.redis = { url: ENV.fetch("REDIS_URL", 'redis://localhost:6379'), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", 'redis://localhost:6379')}
+  config.redis = { url: ENV.fetch("REDIS_URL", 'redis://localhost:6379'), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }}
 end
