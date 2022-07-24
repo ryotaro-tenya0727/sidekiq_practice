8a02ab8e41ef62c862ef6826f3d8699a4ab00c5f
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-24 19:08:00 +0900

    Change: タイムゾーン変更

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module SidekiqPractice
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.active_job.queue_adapter = :sidekiq
-
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
