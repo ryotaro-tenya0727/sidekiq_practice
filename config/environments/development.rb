9c7857c56c5971ae96856b9c64f225db02594ef5
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-25 12:53:17 +0900

    ADD: letteropenerの準備

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   config.cache_classes = false
   host = 'localhost'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Do not eager load code on boot.
   config.eager_load = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   root to: 'messages#new'
   resources :messages
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
 end
