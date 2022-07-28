79347dfb3fc41b285c83222e14e04bc0bfa78dfc
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-28 09:27:16 +0900

    ADD: メールの送信者追加

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,6 +3,6 @@ class MessageMailer < ApplicationMailer
 
   def message_email(message, email)
     @message = message
-    mail(to: email.to_s, subject: "#{message.body}を入力した")
+    mail(to: email.to_s, subject: "#{message.body}を入力した", from: "推しだいありー(公式)")
   end
 end
