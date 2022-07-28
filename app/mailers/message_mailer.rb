47eae408c1ab3f47193ea5b51ea36342888dc175
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-28 09:31:57 +0900

    Change: メールに送信者の名前追加

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,8 +1,8 @@
 class MessageMailer < ApplicationMailer
-  default from: 'from@example.com'
+  default from: email_address_with_name('oshi-diary@example.com', '推しダイアリー（公式）')
 
   def message_email(message, email)
     @message = message
-    mail(to: email.to_s, subject: "#{message.body}を入力した", from: "推しだいありー(公式)")
+    mail(to: email.to_s, subject: "#{message.body}を入力した")
   end
 end
